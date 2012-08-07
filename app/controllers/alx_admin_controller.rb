2af6d7e32feda5700154e1bac85abc985a42f780
alxjrvs <alxjrvs@gmail.com>
2012-08-07 19:53:38 -0400

    git admin renders correctly, added projects

--- app/controllers/alx_admin_controller.rb
+++ app/controllers/alx_admin_controller.rb
@@ -2,7 +2,7 @@ class AlxAdminController < ApplicationController
 
   def index
     if current_user
-      render "Yay"
+      render :text => "Admin!"
     else
       redirect_to new_user_session_path
     end
