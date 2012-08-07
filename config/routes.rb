2c220b408cb39e397d07ce089dabeea6f44fc004
alxjrvs <alxjrvs@gmail.com>
2012-08-07 19:00:00 -0400

    added alx_admin folder and copied all relevant controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ JrvsBlg::Application.routes.draw do
 
   resources :users
 
+  resources :alx_admin
   namespace :alx_admin do
     resources :pages, :posts, :tags
   end
@@ -62,7 +63,7 @@ JrvsBlg::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
