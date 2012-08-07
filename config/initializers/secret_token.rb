d235f3d394aa4df4e79c1286aea2285ba909e550
alxjrvs <alxjrvs@gmail.com>
2012-08-07 12:33:00 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrvsBlg::Application.config.secret_token = 'f930873f66c01e54d6a3d4bf304a61418d4d6fa9559c9bd2f8f9f8b68b07e10c38262e14beb0dc6b8f29ff77fe0658d74f5d328eec58cb31438cdac00f50e6a1'
