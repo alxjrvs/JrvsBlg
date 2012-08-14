7313b9d95d70be9cbf4b35dbddcc933bc115dbff
alxjrvs <alxjrvs@gmail.com>
2012-08-14 18:56:32 -0400

    pagination model in Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < Page
+
+  def next
+    begin
+      self.class.first(:conditions => ["created_at > ?", created_at], :order => "created_at asc")
+    rescue ActiveRecord::RecordNotFound
+      false
+    end
+  end
+
+  def prev
+    begin
+      self.class.first(:conditions => ["created_at < ?", created_at], :order => "created_at desc")
+    rescue ActiveRecord::RecordNotFound
+      false
+    end
+  end
 end
